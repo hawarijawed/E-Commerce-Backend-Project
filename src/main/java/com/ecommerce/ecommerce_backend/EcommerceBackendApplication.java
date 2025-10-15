4d642fafb54d0f2fe33d2951d9446dd63f73efe8
hawarijawed <hawarijawed143@gmail.com>
2025-10-15 13:25:22 +0545

    Adding env file in project

--- src/main/java/com/ecommerce/ecommerce_backend/EcommerceBackendApplication.java
+++ src/main/java/com/ecommerce/ecommerce_backend/EcommerceBackendApplication.java
@@ -1,5 +1,6 @@
 package com.ecommerce.ecommerce_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,11 @@ import java.util.TimeZone;
 public class EcommerceBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kathmandu"));
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 		helper();
